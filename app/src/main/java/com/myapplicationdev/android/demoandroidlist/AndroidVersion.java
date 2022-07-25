b8fee31982925124432552693bbaa32f6aa89c83
RouHui <21016163@myrp.edu.sg>
2022-07-25 20:04:59 +0800

    Initial commit

---
+++ app/src/main/java/com/myapplicationdev/android/demoandroidlist/AndroidVersion.java
@@ -0,0 +1,35 @@
+package com.myapplicationdev.android.demoandroidlist;
+
+public class AndroidVersion {
+
+    private String name;
+    private String version;
+
+    public AndroidVersion(String name, String version) {
+        this.name = name;
+        this.version = version;
+
+
+    }
+
+    @Override
+    public String toString() {
+        return name + ": " + version;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+}
